1951d70d221eb34f3b3cfdd8c07f515e87bdb000
Anastasia Ivanova <amastasia.ivanova@minsk.leveris.com>
2020-10-21 14:59:48 +0300

    feat: add brackets check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = bracketsConfig.flat();
+    let stack = [];
+    let sameBracketsObjArr = bracketsConfig
+        .filter((subArr) => subArr.every((v) => v === subArr[0]))
+        .reduce((objectArr, oldArr) => {
+            let value = Array.from(oldArr)[0];
+            objectArr.push({ conf: value, value: false });
+            return objectArr;
+        }, []);
+
+    for (let bracket of str) {
+        let bracketsIndex = brackets.indexOf(bracket);
+
+        if (bracketsIndex === -1) {
+            return false;
+        }
+
+        if (bracketsIndex !== brackets.lastIndexOf(bracket)) {
+            sameBracketsObjArr.forEach((item, i) => {
+                if (item.conf == bracket) {
+                    if (item.value) {
+                        bracketsIndex++;
+                        item.value = false;
+                    } else {
+                        item.value = true;
+                    }
+                }
+            });
+        }
+
+        if (bracketsIndex % 2 === 0) {
+            stack.push(bracketsIndex + 1);
+        } else {
+            if (stack.pop() !== bracketsIndex) {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
